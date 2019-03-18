b3974706bb969006c435e6aafa1c2a6c68edf5d5
Lord Catzorz <lordcatzorz@users.noreply.github.com>
2019-03-18 15:40:21 -0400

    corrected missing read. (Was junk data)

--- UFS.c
+++ UFS.c
@@ -102,9 +102,10 @@ void printiNode(iNodeEntry iNode) {
 int bd_countusedblocks(void) {
 	int usedblock = 0;
 	char bitmapFreeBlock[BLOCK_SIZE];
+	ReadBlock(FREE_BLOCK_BITMAP, bitmapFreeBlock);
 	for(UINT16 i = BASE_BLOCK_DATA; i < N_BLOCK_ON_DISK; i++)
 	{
-		usedblock += bitmapFreeBlock[i] == 0 ? 0: 1;
+		usedblock += bitmapFreeBlock[i];
 	}
 	return usedblock;
 }
