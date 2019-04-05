0f2d241c190a29f31f786aa184e1ffc4adbd1b4d
Lord Catzorz <lordcatzorz@users.noreply.github.com>
2019-04-05 14:03:58 -0400

    Pour bd_create

--- UFS.c
+++ UFS.c
@@ -658,8 +658,8 @@ int bd_create(const char *pFilename) {
 	fileInodeEntry.iNodeStat.st_blocks = 0;
 	fileInodeEntry.iNodeStat.st_mode = 0;
 	fileInodeEntry.iNodeStat.st_mode |= G_IFREG;
-	endFolderInodeEntry.iNodeStat.st_mode |= G_IRWXU;
-	endFolderInodeEntry.iNodeStat.st_mode |= G_IRWXG;
+	fileInodeEntry.iNodeStat.st_mode |= G_IRWXU;
+	fileInodeEntry.iNodeStat.st_mode |= G_IRWXG;
 
 	if (AddFileInDir(&parentDirectoryINodeEntry, &fileInodeEntry, endFileName) != 0)
 	{
